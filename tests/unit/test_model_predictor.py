23862e629df7489c33daceadede6d21470498fa6
Rocio Gonzalez <rgonzalezt@outlook.com>
2022-09-27 21:33:18 -0300

    si bien los tests que no pasan siguen no pasando, se agregaron tests de variaciones de los enunciados que no pasaban para ver que sí lo hagan

--- tests/unit/test_model_predictor.py
+++ tests/unit/test_model_predictor.py
@@ -21,6 +21,10 @@ def test_resolve_la_x_de_ecuacion_es_ecuacion_explicita(modelo):
     prediccion = modelPredictor.predict("Resolve la x de x+(2+16) +4 = 5")
     assert prediccion == "ecuacion-explicita"
 
+def test_indica_conjunto_solucion_de_la_x_de_ecuacion_es_ecuacion_explicita(modelo):
+    prediccion = modelPredictor.predict("Indicá el conjunto solución de la x de x+(2+16) +4 = 5")
+    assert prediccion == "ecuacion-explicita"
+
 #FIXME
 def test_resolve_la_x_de_ecuacion_compleja_es_ecuacion_explicita(modelo):
     prediccion = modelPredictor.predict("Resolve la x de x*(2+16) +4 = 5")
@@ -47,6 +51,10 @@ def test_analiza_la_funcion_que_pasa_por_puntos_es_funcion_implicita_puntos(mode
     prediccion = modelPredictor.predict("Analiza la funcion que pasa por los puntos (1;2) y (2;1)")
     assert prediccion == "funcion-implicita-puntos"
 
+def test_indica_dominio_imagen_de_la_funcion_que_pasa_por_puntos_es_funcion_implicita_puntos(modelo):
+    prediccion = modelPredictor.predict("Indicá dominio e imagen de la funcion que pasa por los puntos (1;2) y (2;1)")
+    assert prediccion == "funcion-implicita-puntos"
+
 def test_analiza_la_parabola_que_pasa_por_tres_puntos_es_funcion_implicita_puntos(modelo):
     prediccion = modelPredictor.predict("Analiza la parabola que pasa por los puntos (1;2), (2;4) y (3;2)")
     assert prediccion == "funcion-implicita-puntos"
@@ -60,10 +68,18 @@ def test_funcion_que_tiene_pendiente_y_ordenada_es_funcion_implicita_po(modelo):
     prediccion = modelPredictor.predict("Analiza función que tiene ordenada -5 y pendiente 6")
     assert prediccion == "funcion-implicita-po"
 
+def test_indica_dominio_imagen_de_funcion_que_tiene_pendiente_y_ordenada_es_funcion_implicita_po(modelo):
+    prediccion = modelPredictor.predict("Indicá el dominio e imagen de la función que tiene ordenada -5 y pendiente 6")
+    assert prediccion == "funcion-implicita-po"
+
 #FIXME
 def test_oracion_coloquial_es_funcion_implicita(modelo):
     prediccion = modelPredictor.predict("Cuál es la función de x sumado a 10 y multiplicado por 5 es igual a 7?")
-    assert prediccion == "funcion-implicita-po"    
+    assert prediccion == "funcion-implicita"    
+
+def test_determina_oracion_coloquial_es_funcion_implicita(modelo):
+    prediccion = modelPredictor.predict("Determiná la función de x sumado a 10 y multiplicado por 5 es igual a 7?")
+    assert prediccion == "funcion-implicita"    
 
 def test_funcion_que_pasa_por_vertice_es_funcion_implicita_vertice(modelo):
     prediccion = modelPredictor.predict("Qué función tiene su vértice en el punto (2,2) y pasa por (10,6)?")
