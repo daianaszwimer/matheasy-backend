1cb552583403528af84c303348838c20c7a3a340
Ignacio Casciaro <icasciaro@est.frba.utn.edu.ar>
2022-10-24 01:19:34 -0300

    revert changes

--- application.py
+++ application.py
@@ -1,14 +1,10 @@
-from asyncio.log import logger
 from flasgger import swag_from, Swagger
 from flask import Flask, request, jsonify
+
 from src.modelTrainer import model, X_train, y_train
-import watchtower, logging
+
 application = Flask(__name__)
 swagger = Swagger(application)
-
-logging.basicConfig(level=logging.INFO)
-handler = watchtower.CloudWatchLogHandler(log_group_name=application.name)
-application.logger.addHandler(handler)
 
 import src.service as service
 
@@ -34,9 +30,9 @@ def mathtranslation():
 
     try:
         result = service.result(text)
-        logger.info("La expresion matematica del enunciado: " + text + " es: " + result.expression + " y su tag es: " + result.tag)
+        print("La expresion matematica del enunciado: " + text + " es: " + result.expression + " y su tag es: " + result.tag)        
         json = {"result": {"tag": result.tag, "expression": result.expression}}
         return jsonify(json)
     except:
-        logger.info("Error con el enunciado: ", text)
+        print("Error con el enunciado: ", text)
         return jsonify({"error": "An exception occurred"}), 404
