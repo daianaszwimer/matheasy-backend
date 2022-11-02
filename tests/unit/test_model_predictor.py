fd6d13aaa7cf07ad0a1ce4c335a2bd052fed3f47
Rocio Gonzalez <rgonzalezt@outlook.com>
2022-11-01 21:20:02 -0300

    cambio modelo, arregla un test

--- tests/unit/test_model_predictor.py
+++ tests/unit/test_model_predictor.py
@@ -18,7 +18,7 @@ def test_despeja_la_x_de_inecuacion_es_ecuacion_explicita(modelo):
 
 #FIXME
 def test_resolve_la_x_de_ecuacion_es_ecuacion_explicita(modelo):
-    prediccion = modelPredictor.predict("Resolve la x de x+(2+16) +4 = 5")
+    prediccion = modelPredictor.predict("Resolvé la x de x+(2+16) +4 = 5")
     assert prediccion == "ecuacion-explicita"
 
 def test_indica_conjunto_solucion_de_la_x_de_ecuacion_es_ecuacion_explicita(modelo):
@@ -54,7 +54,6 @@ def test_analiza_la_funcion_es_funcion_explicita(modelo):
     prediccion = modelPredictor.predict("Analiza la funcion y = 2*x + 8")
     assert prediccion == "funcion-explicita"
 
-#FIXME
 def test_analiza_la_funcion_que_pasa_por_puntos_es_funcion_implicita_puntos(modelo):
     prediccion = modelPredictor.predict("Analiza la funcion que pasa por los puntos (1;2) y (2;1)")
     assert prediccion == "funcion-implicita-puntos"
@@ -71,7 +70,6 @@ def test_analiza_la_parabola_que_pasa_por_vertice_es_funcion_implicita_vertice(m
     prediccion = modelPredictor.predict("Analiza la parabola que tiene su vertice en (1,2) y pasa por el punto (2,4)")
     assert prediccion == "funcion-implicita-vertice"
 
-#FIXME
 def test_funcion_que_tiene_pendiente_y_ordenada_es_funcion_implicita_po(modelo):
     prediccion = modelPredictor.predict("Analiza la función que tiene ordenada -5 y pendiente 6")
     assert prediccion == "funcion-implicita-po"
@@ -96,3 +94,20 @@ def test_cual_es_la_diferencia_entre_8_y_5_es_ecuacion_implicita(modelo):
     prediccion = modelPredictor.predict("Cual es la diferencia de 8 y 5?")
     assert prediccion == "ecuacion-implicita"  
 
+#FIXME
+def test_f_es_igual_a_una_expresion_es_funcion_explicita(modelo):
+    prediccion = modelPredictor.predict("f(x) = (x - 3)/(x + 2)")
+    assert prediccion == "funcion-explicita"
+
+#FIXME
+def test_y_es_igual_a_una_expresion_es_funcion_explicita(modelo):
+    prediccion = modelPredictor.predict("y = (x - 3)/(x + 2)")
+    assert prediccion == "funcion-explicita"
+
+def test_halla_conjuntos_de_funcion_cuadratica_dado_vertice_y_punto(modelo):
+    prediccion = modelPredictor.predict("Halla función cuadrática tal que vértice = (3; -2) y pase por P = (1; 2). Graficala, halla Co, f(0), C+ y C-.")
+    assert prediccion == "funcion-implicita-vertice"
+
+def test_halla_conjuntos_de_funcion_lineal_dado_puntos(modelo):
+    prediccion = modelPredictor.predict("Halla la función lineal que pasa por P = (3; -2) y Q = (2; -1). Graficala, halla ceros, o. al origen. Indica C+ y C-.")
+    assert prediccion == "funcion-implicita-puntos"
