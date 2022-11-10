1bde84ba4f52ea268305a4b6d42129bd68580083
Ignacio Casciaro <icasciaro@est.frba.utn.edu.ar>
2022-11-09 21:13:27 -0300

    fix: quadratic equation response

--- src/mathgenerators/equationgenerator.py
+++ src/mathgenerators/equationgenerator.py
@@ -3,8 +3,7 @@ from src.enums import Exponent
 
 def generate_equation(exponent):
     if exponent == Exponent.TWO.value:
-        problem, solution = mathgen.quadratic_equation()
-        #problem, solution  = mathgen.expanding()
+        problem, solution = mathgen.quadratic_equation().replace("Zeros of the Quadratic Equation", "")
     else:
         problem, solution  = mathgen.basic_algebra()
     return problem
