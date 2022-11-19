2d23a0defce39dfe618d88e47341991b17cbef59
Rocio Gonzalez <rgonzalezt@outlook.com>
2022-11-19 13:39:37 -0300

    improving ecuacion-explicita regex

--- src/modelPredictor.py
+++ src/modelPredictor.py
@@ -4,8 +4,10 @@ import re
 
 def predict(statement):
     statement_with_no_spaces = statement.replace(" ", "")
-    if bool(re.match(r'\d', statement)) or statement.startswith("x"):
+    if bool(re.match("\(|x|\d|-(\*\d|x|\d)", statement)):
+        print("pasé por aca")
         return "ecuacion-explicita"
     else:
         cleaned_statement = clean_text(statement)
+        print(model.predict([cleaned_statement])[0])
         return model.predict([cleaned_statement])[0]
