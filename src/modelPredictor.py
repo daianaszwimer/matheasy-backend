c6fe64df8893ebc11338020368f11b3ddded0dd0
Rocio Gonzalez <rgonzalezt@outlook.com>
2022-11-20 20:04:10 -0300

    removing prints

--- src/modelPredictor.py
+++ src/modelPredictor.py
@@ -5,9 +5,7 @@ import re
 def predict(statement):
     statement_with_no_spaces = statement.replace(" ", "")
     if bool(re.match("\(|x|\d|-(\*\d|x|\d)", statement)):
-        print("pasé por aca")
         return "ecuacion-explicita"
     else:
         cleaned_statement = clean_text(statement)
-        print(model.predict([cleaned_statement])[0])
         return model.predict([cleaned_statement])[0]
